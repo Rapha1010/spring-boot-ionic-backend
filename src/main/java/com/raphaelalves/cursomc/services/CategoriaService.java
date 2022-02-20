1d1db96ef9786b465da3fbd9e5e20678ba6842e4
Raphael Alves <rapha.tavares1010@gmail.com>
2022-02-20 16:50:08 -0300

    deletetando uma categoria

--- src/main/java/com/raphaelalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/raphaelalves/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.raphaelalves.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.raphaelalves.cursomc.domain.Categoria;
 import com.raphaelalves.cursomc.repositories.CategoriaRepository;
+import com.raphaelalves.cursomc.services.exceptions.DataIntegrityException;
 import com.raphaelalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao é possível excluir um categoria com produtos");
+		}
 	}
 
 }
